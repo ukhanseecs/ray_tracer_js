f1d70f174ae11dbc17fe362878385e5b1c938629
ukhanseecs <ukhan.bee21seecs@seecs.edu.pk>
2025-03-17 11:51:32 +0500

    init commit

---
+++ loop.js
@@ -0,0 +1,24 @@
+let msPrev = window.performance.now();
+const fps = 60;
+const msPerFrame = 1000 / fps;
+
+viewport_width = 800;
+aspect_ratio = 16/9;
+viewport_height = viewport_width / aspect_ratio;
+focal_length = 1;
+
+camera_pos = Vector3D(0, 0, 0);
+
+
+
+function Loop(){
+
+    
+        
+     
+    }
+    //=======================================================================================     
+    
+    
+    Loop();
+    
